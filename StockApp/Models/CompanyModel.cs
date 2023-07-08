0dd3f0b33f89448ef0e205f09bb1643507e14823
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-07-08 12:11:24 -0400

    cik can be a uint instead of a string for memory saving

--- StockApp/Models/CompanyModel.cs
+++ StockApp/Models/CompanyModel.cs
@@ -9,7 +9,7 @@ namespace StockApp.Models
     public class CompanyModel
     {
         
-        private string cik;
+        private uint cik;
 
         
         private string ticker;
@@ -23,9 +23,8 @@ namespace StockApp.Models
         /// <param name="cik"> This is the CIK ID unique ID given to corps in the edgar database</param>
         /// <param name="ticker"> This is the ticker for a company on a stock exchange</param>
         /// <param name="company_name"> This is the full name of a company</param>
-        public CompanyModel(string cik, string ticker, string company_name)
+        public CompanyModel(uint cik, string ticker, string company_name)
         {
-            if (string.IsNullOrEmpty(cik)) throw new ArgumentNullException(nameof(cik));
             if (string.IsNullOrEmpty(ticker)) throw new ArgumentNullException(nameof(ticker));
             if (string.IsNullOrEmpty(company_name)) throw new ArgumentNullException(nameof(company_name));
 
@@ -37,7 +36,7 @@ namespace StockApp.Models
         /// <summary>
         /// CIK index is the unique identifier for a company in the edgar database
         /// </summary>
-        public string CIK { get => cik; set => cik = value; }
+        public uint CIK { get => cik; set => cik = value; }
 
         /// <summary>
         /// Ticker is the symbol of the company on its exchange
